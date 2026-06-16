d659b83375a08cb55bab2807c859cbb80841fc28
maucoronell99 <maucoronel822@gmail.com>
2026-06-15 19:37:30 -0600

    Creación de carpetas

---
+++ generator/password_generator.py
@@ -0,0 +1,23 @@
+import string
+import random
+import string
+
+def generator_password(length=12):
+    characters = (
+        string.ascii_lowercase +
+        string.ascii_uppercase +
+        string.digits +
+        string.punctuation
+    )
+
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
+
+def has_uppercase(password):
+    return any(c.isupper() for c in password)
+
+def has_number(password):
+    return any(c.isdigit() for c in password)
+
+def has_symbol(password):
+    return any(c in string.punctuation for c in password)
